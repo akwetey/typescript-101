2541e03516399ec24bd5d01f1f453fb2d8070afc
Jonathan Akwetey <okinejonathan@yahoo.com>
2020-08-06 15:54:44 +0100

    set up interface to fetch data

--- index.ts
+++ index.ts
@@ -1,1 +1,45 @@
 const el: HTMLElement = document.getElementById("app")!;
+const numberOfPokemon: number = 50;
+
+interface myInterface {
+  id: number;
+  name: string;
+  image: string;
+  type: string;
+}
+const fetchData = (): void => {
+  for (let i = 1; i <= numberOfPokemon; i++) {
+    getData(i);
+  }
+};
+
+const getData = async (id: number): Promise<void> => {
+  const data: Response = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`);
+  const pokemon: any = await data.json();
+  const pokemonType: string = pokemon.types
+    .map((poke: any) => poke.type.name)
+    .join(", ");
+
+  const transformedPokemon = {
+    id: pokemon.id,
+    name: pokemon.name,
+    image: `${pokemon.sprites.front_default}`,
+    type: pokemonType,
+  };
+
+  showPokemon(transformedPokemon);
+};
+
+const showPokemon = (pokemon: myInterface): void => {
+  let output: string = `
+        <div class="card">
+            <span class="card--id">#${pokemon.id}</span>
+            <img class="card--image" src=${pokemon.image} alt=${pokemon.name} />
+            <h1 class="card--name">${pokemon.name}</h1>
+            <span class="card--details">${pokemon.type}</span>
+        </div>
+    `;
+  el.innerHTML += output;
+};
+
+fetchData();
